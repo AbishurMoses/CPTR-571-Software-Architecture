301e3a8667f4bc32b20ec3ca78cb2fba204452d1
Jonathan Stover <jstover@southern.edu>
2026-04-29 15:38:29 -0400

    Redid login/registration page

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react"
+import '../styles/login.css'
 
 interface LoginPageProps {
     setLoggedIn: (value: boolean) => void;
 }
 
 export default function LoginPage({ setLoggedIn }: LoginPageProps) {
+    const [loginRegisterSwitch, setLoginRegisterSwitch] = useState("login");
     const [registerData, setRegisterData] = useState({ username: "", password: "" });
     const [loginData, setLoginData] = useState({ username: "", password: "" });
     const [registerMessage, setRegisterMessage] = useState("");
     const [loginMessage, setLoginMessage] = useState("");
 
+    const handleSwitchClick = async (method: string) => {
+        setLoginRegisterSwitch(method);
+    }
 
     const handleRegisterSubmit = async (e: any) => {
         e.preventDefault();
@@ -44,42 +49,52 @@ export default function LoginPage({ setLoggedIn }: LoginPageProps) {
     };
 
     return (
-        <div id="formGroup">
-            <form id="registerForm" className="form" onSubmit={handleRegisterSubmit}>
-                <h1>Register</h1>
-                <input 
-                    type="text"
-                    placeholder="Username"
-                    value={registerData.username}
-                    onChange={(e) => setRegisterData({ ...registerData, username: e.target.value })}
-                />
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    value={registerData.password}
-                    onChange={(e) => setRegisterData({ ...registerData, password: e.target.value })}
-                />
-                <button type="submit" className="submitBtn">Register</button>
-                <span id="registerResponse">{registerMessage}</span>
-            </form>
+        <>
+            <h1 style={{paddingTop: "50px"}}>The Whatever It's Called Game!</h1>
+            <h3 style={{paddingBottom: "30px"}}>To play, log in or register below.</h3>
 
-            <form id="loginForm" className="form" onSubmit={handleLoginSubmit}>
-                <h1>Log In</h1>
-                <input 
-                    type="text"
-                    placeholder="Username"
-                    value={loginData.username}
-                    onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
-                />
-                <input 
-                    type="password"
-                    placeholder="Password"
-                    value={loginData.password}
-                    onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
-                />
-                <button type="submit" className="submitBtn">Log In</button>
-                <span id="loginResponse">{loginMessage}</span>
-            </form>
-        </div>
+            <div className="switchContainer">
+                <span className={`loginSwitch ${loginRegisterSwitch === "login" ? "selectedSwitch" : ""}`} onClick={() => handleSwitchClick("login")}>Log in</span>
+                <span className={`registerSwitch ${loginRegisterSwitch === "register" ? "selectedSwitch" : ""}`} onClick={() => handleSwitchClick("register")}>Register</span>
+            </div>
+
+            <div id="formGroup">
+                <form id="registerForm" className={`form ${loginRegisterSwitch === "register" ? "selectedForm" : "slide-right"}`} onSubmit={handleRegisterSubmit}>
+                    <h1>Register</h1>
+                    <input 
+                        type="text"
+                        placeholder="Username"
+                        value={registerData.username}
+                        onChange={(e) => setRegisterData({ ...registerData, username: e.target.value })}
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        value={registerData.password}
+                        onChange={(e) => setRegisterData({ ...registerData, password: e.target.value })}
+                    />
+                    <button type="submit" className="submitBtn">Register</button>
+                    <span id="registerResponse">{registerMessage}</span>
+                </form>
+
+                <form id="loginForm" className={`form ${loginRegisterSwitch === "login" ? "selectedForm" : "slide-left"}`} onSubmit={handleLoginSubmit}>
+                    <h1>Log In</h1>
+                    <input 
+                        type="text"
+                        placeholder="Username"
+                        value={loginData.username}
+                        onChange={(e) => setLoginData({ ...loginData, username: e.target.value })}
+                    />
+                    <input 
+                        type="password"
+                        placeholder="Password"
+                        value={loginData.password}
+                        onChange={(e) => setLoginData({ ...loginData, password: e.target.value })}
+                    />
+                    <button type="submit" className="submitBtn">Log In</button>
+                    <span id="loginResponse">{loginMessage}</span>
+                </form>
+            </div>
+        </>
     );
 }
